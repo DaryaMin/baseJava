fb650479971fa32e0cc75c1b0369b4406b31e397
dminakova <dminakova@cinimex.ru>
2021-12-28 17:41:01 +0300

    first lesson

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,3 +1,6 @@
+import java.util.Arrays;
+import java.util.Objects;
+
 /**
  * Array based storage for Resumes
  */
@@ -5,26 +8,58 @@ public class ArrayStorage {
     Resume[] storage = new Resume[10000];
 
     void clear() {
+        Arrays.fill(storage, 0, size()+1, null);
     }
 
     void save(Resume r) {
+        storage[size()] = r;
     }
 
     Resume get(String uuid) {
+        try {
+            for (Resume resume : storage) {
+                if (resume.uuid.equals(uuid)) {
+                    return resume;
+                }
+            }
+        } catch (NullPointerException e) {
+            System.out.println("Резюме с uuid = " + uuid + " не существует");
+        }
         return null;
     }
 
     void delete(String uuid) {
+        int numberForDelete = 0;
+
+        for (Resume resume : storage) {
+            if (resume.toString() == uuid) {
+                resume = null;
+                break;
+            }
+            numberForDelete++;
+
+        }
+        for (int i = numberForDelete; i <= size(); i++) {
+            storage[i] = storage[i+1];
+            storage[i + 1] = null;
+        }
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        return Arrays.copyOf(storage, size());
     }
 
     int size() {
-        return 0;
+        int size = 0;
+        for (Resume resume : storage) {
+            if (resume == null) {
+                break;
+            }
+            size++;
+        }
+        return size;
     }
 }
