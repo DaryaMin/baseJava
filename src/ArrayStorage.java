edf701f7169b9fc6d4ed59d675c3d39e7b245753
dminakova <dminakova@cinimex.ru>
2021-12-28 18:02:12 +0300

    change NPE, add parameter size

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -5,26 +5,27 @@ import java.util.Arrays;
  */
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
+    int size = 0;
 
     void clear() {
         Arrays.fill(storage, 0, size() + 1, null);
+        size = 0;
     }
 
     void save(Resume r) {
         storage[size()] = r;
+        size++;
     }
 
     Resume get(String uuid) {
-        try {
-            for (Resume resume : storage) {
-                if (resume.uuid.equals(uuid)) {
+        for (Resume resume : storage) {
+            if (resume != null) {
+                if (resume.toString().equals(uuid)) {
                     return resume;
                 }
             }
-        } catch (NullPointerException e) {
-            System.out.println("Резюме с uuid = " + uuid + " не существует");
         }
-        return null;
+            return null;
     }
 
     void delete(String uuid) {
@@ -38,27 +39,21 @@ public class ArrayStorage {
             numberForDelete++;
 
         }
-        for (int i = numberForDelete; i <= size(); i++) {
+        for (int i = numberForDelete; i <= size; i++) {
             storage[i] = storage[i + 1];
             storage[i + 1] = null;
         }
+        size--;
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return Arrays.copyOf(storage, size());
+        return Arrays.copyOf(storage, size);
     }
 
     int size() {
-        int size = 0;
-        for (Resume resume : storage) {
-            if (resume == null) {
-                break;
-            }
-            size++;
-        }
         return size;
     }
 }
