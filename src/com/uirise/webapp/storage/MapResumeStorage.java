d13270f47e8d5e2e4c45ebe5131f7639f2acc34f
dminakova <dminakova@cinimex.ru>
2022-01-24 15:46:12 +0300

    HW6 MapResumeStorage

---
+++ src/com/uirise/webapp/storage/MapResumeStorage.java
@@ -0,0 +1,58 @@
+package com.uirise.webapp.storage;
+
+import com.uirise.webapp.model.Resume;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class MapResumeStorage extends AbstractStorage {
+    private final Map<String, Resume> storage = new HashMap<>();
+
+    @Override
+    protected Resume getSearchKey(String uuid) {
+        return storage.get(uuid);//Возвращает Resume
+    }
+
+    @Override
+    protected boolean isExist(Object searchKey) {
+        return storage.containsValue((Resume) searchKey);
+    }
+
+    @Override
+    protected void doUpdate(Resume resume, Object searchKey) {
+        storage.replace(searchKey.toString(), resume);
+    }
+
+    @Override
+    protected void doSave(Resume r, Object searchKey) {
+        storage.put(r.getUuid(), r);
+    }
+
+    @Override
+    protected Resume doGet(Object searchKey) {
+        return storage.get(searchKey.toString());
+    }
+
+    @Override
+    protected void doDelete(Object searchKey) {
+        storage.remove(searchKey.toString());
+    }
+
+    @Override
+    public void clear() {
+        storage.clear();
+    }
+
+    @Override
+    public List<Resume> doGetAll() {
+        return new ArrayList<>(storage.values());
+    }
+
+    @Override
+    public int size() {
+        return storage.size();
+    }
+}
+
