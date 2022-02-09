fc5013d72f9169f3a89976afc1239e2df36a6f9c
dminakova <dminakova@cinimex.ru>
2022-02-09 19:29:35 +0300

    HW08 add printFileName

--- src/com/uirise/webapp/MainFile.java
+++ src/com/uirise/webapp/MainFile.java
@@ -3,6 +3,8 @@ package com.uirise.webapp;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class MainFile {
     public static void main(String[] args) {
@@ -15,12 +17,13 @@ public class MainFile {
             throw new RuntimeException("Error", e);
         }
 
-        File dir = new File("./src/ru/javawebinar/basejava");
+        File dir = new File("C:\\D\\QA\\java\\basejava");
         System.out.println(dir.isDirectory());
         String[] list = dir.list();
         if (list != null) {
             for (String name : list) {
                 System.out.println(name);
+
             }
         }
 
@@ -29,5 +32,19 @@ public class MainFile {
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+        printFileNames(".\\src");
+    }
+
+    public static void printFileNames(String sDir){
+        File[] files = new File(sDir).listFiles();
+        for(File file: files){
+            if(file != null){
+                System.out.println(file.getName());
+            }
+            if(file.isDirectory()){
+                printFileNames(file.getAbsolutePath());
+            }
+        }
     }
 }
+
