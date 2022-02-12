ec565690f145d82b90f0f28d0c50ba02a39b3161
dminakova <dminakova@cinimex.ru>
2022-02-12 22:26:28 +0300

    HW08 edit

--- test/com/uirise/webapp/storage/MapStorageTest.java
+++ test/com/uirise/webapp/storage/MapStorageTest.java
@@ -2,6 +2,6 @@ package com.uirise.webapp.storage;
 
 public class MapStorageTest extends AbstractStorageTest{
     public MapStorageTest() {
-        super(new MapStorage());
+        super(new MapUuidStorage());
     }
 }
