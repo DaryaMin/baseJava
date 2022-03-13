ee8a025d75b753d74f959c130069628f4a0268e0
dminakova <dminakova@cinimex.ru>
2022-03-13 10:55:17 +0300

    HW11 deadlock

--- src/com/uirise/webapp/MainConcurrency.java
+++ src/com/uirise/webapp/MainConcurrency.java
@@ -1,7 +1,9 @@
 package com.uirise.webapp;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class MainConcurrency {
     public static final int THREADS_NUMBER = 10000;
@@ -58,6 +60,35 @@ public class MainConcurrency {
             }
         });
         System.out.println(mainConcurrency.counter);
+
+        //ДЗ по deadlock
+        Map<String, String> bill1 = new HashMap<>();
+        bill1.put("id", "uuid1");
+        bill1.put("count", "12234");
+        Map<String, String> bill2 = new HashMap<>();
+        bill2.put("id", "uuid2");
+        bill2.put("count", "122345");
+
+        send(bill1, bill2);
+        send(bill2, bill1);
+
+
+    }
+
+    private static void send(Map<String, String> billFrom, Map<String, String> billTo) {
+        new Thread(() -> {
+            synchronized (billFrom) {
+                System.out.println("Перевод с " + billFrom.get("id"));
+                try {
+                    Thread.sleep(50);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                synchronized (billTo) {
+                    System.out.println("Перевод на " + billTo.get("id"));
+                }
+            }
+        }).start();
     }
 
     private synchronized void inc() {
