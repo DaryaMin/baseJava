1eae51dc6b2bc4d531860e6064a555db5c5cff08
dminakova <dminakova@cinimex.ru>
2022-04-13 09:47:36 +0300

    HW17

--- src/com/uirise/webapp/Config.java
+++ src/com/uirise/webapp/Config.java
@@ -4,17 +4,15 @@ import com.uirise.webapp.sql.SqlStorage;
 import com.uirise.webapp.storage.Storage;
 
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
 
 public class Config {
-    private static final File PROPS = new File("C:\\D\\QA\\java\\basejava\\config\\resumes.properties");
+    private static final String PROPS = "resumes.properties";
     private static final Config INSTANCE = new Config();
 
-    private Properties props = new Properties();
-    private File storageDir;
+    private final File storageDir;
     private final Storage storage;
 
     public static Config get() {
@@ -22,12 +20,13 @@ public class Config {
     }
 
     private Config() {
-        try (InputStream is = new FileInputStream(PROPS)) {
+        try (InputStream is = Config.class.getResourceAsStream(PROPS)) {
+            Properties props = new Properties();
             props.load(is);
             storageDir = new File(props.getProperty("storage.dir"));
             storage = new SqlStorage(props.getProperty("db.url"), props.getProperty("db.user"), props.getProperty("db.password"));
         } catch (IOException e) {
-            throw new IllegalStateException("Invalid config file " + PROPS.getAbsolutePath());
+            throw new IllegalStateException("Invalid config file " + PROPS);
         }
     }
 
